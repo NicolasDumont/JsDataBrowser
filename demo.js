e6f5d30b4c6ee2977afb355390f91c83ade4ec5e
NicolasDumont <d.nicolas.d@gmail.com>
2014-08-05 22:48:46 +0200

    DataLayout ok (one array by discovered line type to be implemented). XLS/XSLX parsing ok, to be cleaned and inserted into dataLayout object.

--- demo.js
+++ demo.js
@@ -1,4 +1,8 @@
-//PARSER:
+//XLS PARSER:
+var drop;
+var XL;
+
+//CSV PARSER:
 var stepped = 0, rowCount = 0, errorCount = 0, firstError;
 var start, end;
 //var firstRun = true;
@@ -33,33 +37,35 @@ var parseConfig = {
  * arr.splice(Math.abs(arr.binaryIndexOf(3)), 0, 3);
  * document.body.textContent = JSON.stringify(arr);
  */
-binaryIndexOf=function (key) {
+binaryIndexOf = function (lookupValue) {
     'use strict';
 
-    var low = 0;
-    var high = this.length - 1;
-    var mid;
-    var midVal;
+    var lowIndex = 0;
+    var highIndex = this.length - 1;
+    var midIndex;
+    var midValue;
 
-    while (low <= high) {
-        mid = (low + high) >>> 1;  //int mid = low + ((high - low) / 2);
-        midVal = this[mid];
-        console.warn("low: " + low + ", high: " + high + ", mid: " + mid + ", midVal: " + midVal);
+    while (lowIndex <= highIndex) {
+        midIndex = (lowIndex + highIndex) >>> 1;  //http://www.c-point.com/javascript_tutorial/jsoprurshift.htm //int mid = low + ((high - low) / 2);
+        midValue = this[midIndex];
+        console.warn("lowIndex: " + lowIndex + ", highIndex: " + highIndex + ", midIndex: " + midIndex + ", midValue: " + midValue);
 
-        if (midVal < key) {
-            low = mid+1;
+        if (midValue < lookupValue) {
+            lowIndex = midIndex + 1;
         }
-        else if (midVal > key) {
-            high = mid-1;
+        else if (midValue > lookupValue) {
+            highIndex = midIndex - 1;
         }
         else {
-            console.warn("KEY FOUND at: " + mid);
-            return mid;
+            console.warn("KEY FOUND at: " + midIndex);
+            return midIndex;
         }
     }
-    console.warn("KEY WAS NEVER FOUND, but it *would* be at: " + mid + " min: " + low + " max: " + high + ". We found: " + midVal + " at that index, and are looking for " + key);
-    //return ~Math.max(low, high);
-    return -(low + 1);  // key not found
+    //return ~Math.max(lowIndex, highIndex);
+    //return -(lowIndex + 1);  // key not found
+    var returnValue = lowIndex - 1;
+    console.warn("KEY WAS NEVER FOUND, but it *would* be at index " + midIndex + ". MinIndex: " + lowIndex + ". MaxIndex: " + highIndex + ". We found value " + midValue + " at index " + highIndex + " while looking for " + lookupValue + ". Returning " + returnValue);
+    return returnValue;
 }
 /*public static int binarySearch(int[] a, int key) {
      int low = 0;
@@ -101,8 +107,8 @@ var DataLayout = {
 };
 var dataLayout = Object.create(DataLayout);
 //var loadDataLayout = function(excelDataLayoutFile) {
-var item1 = new DataItem("field1", "This is field number one.", 0, 2);
-dataLayout.addItem(item1);
+//var item1 = new DataItem("field1", "This is field number one.", 0, 2);
+//dataLayout.addItem(item1);
     dataLayout.addItem(new DataItem("field1", "This is field number one.", 0, 2));
     dataLayout.addItem(new DataItem("field2", "This is field number two.", 2, 5));
 //}
@@ -165,8 +171,15 @@ $(function()
 	$('#remote-malformed-file').click(function() {
 		$('#url').val($('#local-malformed-file').attr('href'));
 	});*/
+    //XLS PARSER:
+    drop = document.getElementById('drop');
+    if(drop.addEventListener) {
+        drop.addEventListener('dragenter', handleDragover, false);
+        drop.addEventListener('dragover', handleDragover, false);
+        drop.addEventListener('drop', handleDrop, false);
+    }
 
-    //PARSER:
+    //CSV PARSER:
 	$('#submit').click(function()
 	{
 		if ($(this).prop('disabled') == "true")
@@ -262,16 +275,28 @@ $(function()
         var rowStart = editor.selection.getCursor().row;
         var rowEnd = editor.selection.getCursor().row;
         var columnStart = editor.selection.getCursor().column;
-        var columnEnd = editor.selection.getCursor().column + 2;
+        var columnEnd;
+        var tooltipText;
+        var foundItem = dataLayout.getItem(columnStart);
+        if (typeof foundItem !== "undefined" && columnStart < foundItem.position + foundItem.length) {
+            //alert(foundItem.description);
+            columnStart = foundItem.position;
+            columnEnd = foundItem.position + foundItem.length;
+            textMarker = editor.session.addMarker(new aceRange(rowStart, columnStart, rowEnd, columnEnd), "ace_selection", "text"); //"ace_active-line", "fullLine");
+            tooltipText = foundItem.description;
+        }
+        else {
+            //alert("Item not defined in the data layout!");
+            tooltipText = "No corresponding item defined in Data Layout!";
+        }
         //var range = new Range(rowStart, columnStart, rowEnd, columnEnd);
         //marker = editor.getSession().addMarker(range,"ace_selected_word", "text");
         //state.highlightMarker = session.addMarker(adjRangeAce,"ace_selection", "text");
-        textMarker = editor.session.addMarker(new aceRange(rowStart, columnStart, rowEnd, columnEnd), "ace_selection", "text"); //"ace_active-line", "fullLine");
         //editor.tokenTooltip.show(editor.text, 10, 10);
         //var s = editor.getSelectedText();
         //if (s != "")
-        editor.tokenTooltip.setText(editor.getSelectedText());
-        alert(dataLayout.getItem(columnStart).description);
+        //editor.tokenTooltip.setText(editor.getSelectedText());
+        editor.tokenTooltip.setText(tooltipText);
     });
     /*editor.commands.addCommand({
         name: 'myCommand',
@@ -323,7 +348,205 @@ function completeFn(results)
     editor.setValue(csv);
 }
 
-function hoveredItem()
-{
-
+//XLS PARSER
+function fixdata(data) {
+    var o = "", l = 0, w = 10240;
+    for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint8Array(data.slice(l*w,l*w+w)));
+    o+=String.fromCharCode.apply(null, new Uint8Array(data.slice(o.length)));
+    return o;
 }
+
+function to_json(workbook) {
+    var result = {};
+    workbook.SheetNames.forEach(function(sheetName) {
+        var roa = XLS.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+        if(roa.length > 0){
+            result[sheetName] = roa;
+        }
+    });
+    return result;
+}
+
+function to_csv(workbook) {
+    var result = [];
+    workbook.SheetNames.forEach(function(sheetName) {
+        var csv = XLS.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+        if(csv.length > 0){
+            result.push("SHEET: " + sheetName);
+            result.push("");
+            result.push(csv);
+        }
+    });
+    return result.join("\n");
+}
+
+function to_csv(workbook) {
+    var result = [];
+    workbook.SheetNames.forEach(function(sheetName) {
+        var csv = XLS.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+        if(csv.length > 0){
+            result.push("SHEET: " + sheetName);
+            result.push("");
+            result.push(csv);
+        }
+    });
+    return result.join("\n");
+}
+
+function to_formulae(workbook) {
+    var result = [];
+    workbook.SheetNames.forEach(function(sheetName) {
+        var formulae = XLS.utils.get_formulae(workbook.Sheets[sheetName]);
+        if(formulae.length > 0){
+            result.push("SHEET: " + sheetName);
+            result.push("");
+            result.push(formulae.join("\n"));
+        }
+    });
+    return result.join("\n");
+}
+
+var tarea = document.getElementById('b64data');
+function b64it() {
+    if(typeof console !== 'undefined') console.log("onload", new Date());
+    var wb = XLS.read(tarea.value, {type: 'base64'});
+    process_wb(wb);
+}
+
+function process_wb(wb) {
+    if(use_worker) XLS.SSF.load_table(wb.SSF);
+    var output = "";
+    /*switch(get_radio_value("format")) {
+        case "json":
+            output = JSON.stringify(to_json(wb), 2, 2);
+            break;
+        case "form":
+            output = to_formulae(wb);
+            break;
+        default:*/
+            output = to_csv(wb);
+    output = to_html(wb);
+    //}
+    if(out.innerText === undefined) out.textContent = output;
+    else out.innerText = output;
+    if(typeof console !== 'undefined') console.log("output", new Date());
+}
+
+function handleDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    rABS = 1;
+    use_worker = 0;
+    var files = e.dataTransfer.files;
+    var i, f;
+    for (i = 0, f = files[i]; i != files.length; ++i) {
+        var reader = new FileReader();
+        var name = f.name;
+        reader.onload = function(e) {
+            if(typeof console !== 'undefined') console.log("onload", new Date(), rABS, use_worker);
+            var data = e.target.result;
+            //var arr = new Array();
+            //for(var i = 0; i != data.length; ++i) arr[i] = data[i];
+            var fileExtension = f.name.split('.').pop();
+            var wb;
+            if(fileExtension === "xls") //arr[0] == 0xd0)
+                //wb = (XL=XLS).read(arr, {type:"array"});
+                wb = (XL=XLS).read(data, {type: 'binary'});
+            else if (fileExtension === "xlsx")
+                //wb = (XL=XLSX).read(arr, {type:"binary"});
+                wb = (XL=XLSX).read(data, {type:"binary"});
+            else
+                alert("Please upload a xls or a xlsx file!")
+            console.log(wb);
+            process_wb(wb); //, fmt);
+
+            /* from http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript */
+            /*function getParameterByName(name) {
+                name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                    results = regex.exec(location.search);
+                return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+            }
+
+            var url=getParameterByName("file");
+            var fmt=getParameterByName("fmt");
+            var oReq = new XMLHttpRequest();
+            oReq.open("GET", url, true);
+            oReq.responseType = "arraybuffer";
+            oReq.onload = function(e) {
+                var arraybuffer = oReq.response;
+                var data = new Uint8Array(arraybuffer);
+                var arr = new Array();
+                for(var i = 0; i != data.length; ++i) arr[i] = data[i];
+                var wb;
+                if(arr[0] == 0xd0) wb = (XL=XLS).read(arr, {type:"array"});
+                else wb = (XL=XLSX).read(arr, {type:"binary"});
+                console.log(wb);
+                process_wb(wb, fmt);
+            }
+            oReq.send();*/
+            /*if(use_worker) {
+                xlsworker(data, process_wb);
+            } else {
+                var wb;
+                if(rABS) {
+                    wb = XLS.read(data, {type: 'binary'});
+                } else {
+                    var arr = fixdata(data);
+                    wb = XLS.read(btoa(arr), {type: 'base64'});
+                }
+                process_wb(wb);
+            }*/
+        };
+        if(rABS) reader.readAsBinaryString(f);
+        else reader.readAsArrayBuffer(f);
+    }
+}
+
+function handleDragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+}
+
+//Table Display:
+function get_columns(sheet) {
+    var val, rowObject, range, columnHeaders, emptyRow, C;
+    range = XL.utils.decode_range(sheet["!ref"]);
+    columnHeaders = [];
+    for (C = range.s.c; C <= range.e.c; ++C) {
+        val = sheet[XL.utils.encode_cell({c: C, r: range.s.r})];
+        if(val){
+            switch(val.t) {
+                case 's': case 'str': columnHeaders[C] = val.v; break;
+                case 'n': columnHeaders[C] = val.v; break;
+            }
+        }
+    }
+    return columnHeaders;
+}
+
+function to_html(wb) {
+    var json = to_json(wb);
+    wb.SheetNames.forEach(function(sheet) {
+        var cols = get_columns(wb.Sheets[sheet]);
+        var tbl = document.createElement('div');
+        var src = "<h3>" + sheet + "</h3>";
+        src += "<table>";
+        src += "<thead><tr>";
+        cols.forEach(function(c) { src += "<th>" + (typeof c !== "undefined" ? c : "") + "</th>"; });
+        src += "</tr></thead>";
+        (json[sheet]||[]).forEach(function(row) {
+            src += "<tr>";
+            cols.forEach(function(c) { src += "<td>" + (typeof row[c] !== "undefined" ? row[c] : "") + "</c>"; });
+            src += "</tr>";
+        });
+        src += "</table>";
+        tbl.innerHTML = src;
+        var node = document.getElementById('hot');
+        while (node.hasChildNodes()) {
+            node.removeChild(node.firstChild);
+        }
+        node.appendChild(tbl);
+    });
+};
